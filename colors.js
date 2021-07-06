3e52af9b89b09fb00b99839047d148c01d66936c
Nirosha55 <59763107+Nirosha55@users.noreply.github.com>
2021-07-06 11:25:12 +0530

    initial commit

---
+++ colors.js
@@ -0,0 +1,9 @@
+
+
+export const WHITE = '#FFFFFF';
+export const BLACK = '#000000';
+export const LIGHT_GRAY = "#f7f7f7";
+export const DARK_GRAY = "#1e2022";
+export const GRAY = "gray"
+export const RED = "#ff0000";
+export const TEXT_INPUT_BORDER_COLOR = "#000000"
